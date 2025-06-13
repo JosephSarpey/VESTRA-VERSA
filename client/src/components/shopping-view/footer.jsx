0b2db1c511614a99c48534d01f0fe110ecb27935
JosephSarpey <josephsarpey2002@gmail.com>
2025-06-14 00:35:14 +0530

    ...

--- client/src/components/shopping-view/footer.jsx
+++ client/src/components/shopping-view/footer.jsx
@@ -1,4 +1,4 @@
-import { FaFacebook, FaInstagram, FaTwitter } from "react-icons/fa";
+import { FaFacebook, FaInstagram, FaTwitter, FaWhatsapp } from "react-icons/fa";
 import { Link } from "react-router-dom";
 
 function Footer() {
