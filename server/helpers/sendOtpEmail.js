11c5c9db2c29a2bc268f093b6e2e12398b14b84d
JosephSarpey <josephsarpey2002@gmail.com>
2025-06-24 01:07:48 +0530

    OTP mail bug fix

--- server/helpers/sendOtpEmail.js
+++ server/helpers/sendOtpEmail.js
@@ -6,7 +6,9 @@ const sendOtpEmail = async (email, otp) => {
 
   try {
     const transporter = nodemailer.createTransport({
-      service: 'gmail',
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
+      secure: true,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
